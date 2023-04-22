0a933c67b68a0905c9696fb45370b71cab5a8193
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-04-22 01:47:00 -0300

    Fixing compiling errors in CreateRelationsBoard.hs

--- src/CreateRelationsBoard.hs
+++ src/CreateRelationsBoard.hs
@@ -7,44 +7,30 @@ createRelationsBoard sizeBoard = do
     if sizeBoard == 4
         then do 
             putStrLn "Informe as relações de cada bloco para o tabuleiro de tamanho 4x4:"
-            row1 :: [Char]
-            row2 :: [Char]
-            row3 :: [Char]
-            row4 :: [Char]
             row1 <- getLine
             row2 <- getLine
             row3 <- getLine
             row4 <- getLine
-            return ()
+            let allRows = [row1, row2, row3, row4]
+            let relationsBoard = getRelationsBoard allRows
+            print relationsBoard
 
     else if sizeBoard == 6
         then do 
             putStrLn "Informe as relações de cada bloco para o tabuleiro de tamanho 6x6:"
-            row1 :: [Char]
-            row2 :: [Char]
-            row3 :: [Char]
-            row4 :: [Char]
-            row5 :: [Char]
-            row6 :: [Char]
             row1 <- getLine
             row2 <- getLine
             row3 <- getLine
             row4 <- getLine
             row5 <- getLine
             row6 <- getLine
-            return ()
+            let allRows = [row1, row2, row3, row4, row5, row6]
+            let relationsBoard = getRelationsBoard allRows
+            print relationsBoard
+
     else if sizeBoard == 9
         then do
             putStrLn "Informe as relações de cada bloco para o tabuleiro de tamanho 9x9:"
-            row1 :: [Char]
-            row2 :: [Char]
-            row3 :: [Char]
-            row4 :: [Char]
-            row5 :: [Char]
-            row6 :: [Char]
-            row7 :: [Char]
-            row8 :: [Char]
-            row9 :: [Char]
             row1 <- getLine
             row2 <- getLine
             row3 <- getLine
@@ -54,27 +40,21 @@ createRelationsBoard sizeBoard = do
             row7 <- getLine
             row8 <- getLine
             row9 <- getLine
-            return ()
+            let allRows = [row1, row2, row3, row4, row5, row6, row7, row8, row9]
+            let relationsBoard = getRelationsBoard allRows
+            print relationsBoard
     else
         putStrLn "As dimensões do tabuleiro fornecidas são inválidas."
-    
-    allRows :: Int -> [[Char]]
-    allRows size
-        | size == 4 = [row1, row2, row3, row4]
-        | size == 6 = [row1, row2, row3, row4, row5, row6]
-        | size == 9 = [row1, row2, row3, row4,row5, row6, row7, row8, row9]
-        | otherwise = error "Invalid size!"
 
-    getPipeOut :: [Char] -> [Char]
-    getPipeOut [] = []
-    getPipeOut (x:xs) = if x /= '|' then x : takePipeOut xs else takePipeOut xs
+getPipeOut :: [Char] -> [Char]
+getPipeOut [] = []
+getPipeOut (x:xs) = if x /= '|' then x : getPipeOut xs else []
 
+getAllRelationsFromRow :: [Char] -> [[Char]]
+getAllRelationsFromRow [] = []
+getAllRelationsFromRow rawRow = take 4 row : getAllRelationsFromRow (drop (4+1) row) -- 4 IS HARDCORDED HERE (NEED TO BE FIXED)
+    where
+        row = getPipeOut rawRow
 
-    getAllRelationsFromRow :: [Char] -> [[Char]]
-    getAllRelationsFromRow [] = []
-    getAllRelationsFromRow rawRow = take sizeBoard row : takeAllComparatorsFromRow (drop sizeBoard row)
-                                        where
-                                          row = getPipeOut rawRow
-
-    getRelationsBoard :: [[[Char]]]
-    getRelationsBoard = map getAllRelationsFromRow (allRows sizeBoard)
+getRelationsBoard :: [[Char]] -> [[[Char]]]
+getRelationsBoard allRows = map getAllRelationsFromRow allRows
