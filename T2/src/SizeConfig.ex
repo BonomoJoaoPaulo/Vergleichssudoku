78dde1016938bf4ee832463fc3d297c08d18207a
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-05-26 14:01:53 -0300

    'Translate' SizeConfig from Haskell

---
+++ T2/src/SizeConfig.ex
@@ -0,0 +1,9 @@
+defmodule SizeConfig do
+  @size_board 6
+  @size_row_region 3
+  @size_column_region 2
+
+  def size_board, do: @size_board
+  def size_row_region, do: @size_row_region
+  def size_column_region, do: @size_column_region
+end
