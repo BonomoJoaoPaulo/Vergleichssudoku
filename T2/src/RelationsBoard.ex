af207e98e91a62f7ed7ff078c869e133b12896f7
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-05-26 18:07:43 -0300

    Some changes in RelationsBoard structure

--- T2/src/RelationsBoard.ex
+++ T2/src/RelationsBoard.ex
@@ -1,35 +1,36 @@
-alias SizeConfig, as: SizeConfig
-
 defmodule RelationsBoard do
   def get_relations do
     board_size = SizeConfig.size_board()
 
+    row1size4 = ".<>.|..<>|.<<.|..>>|"
+    row2size4 = "<<..|>..>|>>..|<..<|"
+    row3size4 = ".>>.|..<<|.<<.|..>>|"
+    row4size4 = "<>..|>..<|>>..|<..<|"
+
+    row1size6 = ".<<.|..>>|.><.|..<<|.><.|..><|"
+    row2size6 = ">>>.|<.><|>>>.|>.<<|>><.|<.<<|"
+    row3size6 = "<<..|<..>|<<..|>..>|><..|>..>|"
+    row4size6 = ".<<.|..<>|.<>.|..>>|.<<.|..>>|"
+    row5size6 = ">>>.|>.><|<<<.|<.<>|><<.|<.>>|"
+    row6size6 = "<>..|<..<|><..|>..>|>>..|<..<|"
+
+    row1size9 = ".<>.|.><>|..<<|.<>.|.<>>|..>>|.><.|.>><|..<<|"
+    row2size9 = "<<<.|><<>|>.<>|<<<.|<>>>|<.><|>>>.|<<><|>.>>|"
+    row3size9 = "><..|><.>|>..>|><..|<>.>|<..<|<>..|<<.<|<..>|"
+    row4size9 = ".>>.|.><<|..<<|.>>.|.<><|..>>|.<<.|.>>>|..<<|"
+    row5size9 = "<>>.|><<<|>.<>|<><.|<<<<|<.>>|>>>.|<>><|>.><|"
+    row6size9 = "<<..|>>.>|>..<|><..|>>.>|<..<|<>..|<<.<|<..>|"
+    row7size9 = ".<>.|.>>>|..><|.><.|.>><|..><|.<<.|.><>|..<<|"
+    row8size9 = "<><.|<<<<|<.>>|>>>.|<<><|<.<>|><>.|><<>|>.>>|"
+    row9size9 = "><..|>>.>|<..<|<<..|<<.>|>..>|<<..|>>.>|<..<|"
+
     if board_size == 4 do
-      row1size4 = ".<>.|..<>|.<<.|..>>|"
-      row2size4 = "<<..|>..>|>>..|<..<|"
-      row3size4 = ".>>.|..<<|.<<.|..>>|"
-      row4size4 = "<>..|>..<|>>..|<..<|"
       [row1size4, row2size4, row3size4, row4size4]
     else
       if board_size == 6 do
-        row1size6 = ".<<.|..>>|.><.|..<<|.><.|..><|"
-        row2size6 = ">>>.|<.><|>>>.|>.<<|>><.|<.<<|"
-        row3size6 = "<<..|<..>|<<..|>..>|><..|>..>|"
-        row4size6 = ".<<.|..<>|.<>.|..>>|.<<.|..>>|"
-        row5size6 = ">>>.|>.><|<<<.|<.<>|><<.|<.>>|"
-        row6size6 = "<>..|<..<|><..|>..>|>>..|<..<|"
         [row1size6, row2size6, row3size6, row4size6, row5size6, row6size6]
       else
         if board_size == 9 do
-          row1size9 = ".<>.|.><>|..<<|.<>.|.<>>|..>>|.><.|.>><|..<<|"
-          row2size9 = "<<<.|><<>|>.<>|<<<.|<>>>|<.><|>>>.|<<><|>.>>|"
-          row3size9 = "><..|><.>|>..>|><..|<>.>|<..<|<>..|<<.<|<..>|"
-          row4size9 = ".>>.|.><<|..<<|.>>.|.<><|..>>|.<<.|.>>>|..<<|"
-          row5size9 = "<>>.|><<<|>.<>|<><.|<<<<|<.>>|>>>.|<>><|>.><|"
-          row6size9 = "<<..|>>.>|>..<|><..|>>.>|<..<|<>..|<<.<|<..>|"
-          row7size9 = ".<>.|.>>>|..><|.><.|.>><|..><|.<<.|.><>|..<<|"
-          row8size9 = "<><.|<<<<|<.>>|>>>.|<<><|<.<>|><>.|><<>|>.>>|"
-          row9size9 = "><..|>>.>|<..<|<<..|<<.>|>..>|<<..|>>.>|<..<|"
           [row1size9, row2size9, row3size9, row4size9, row5size9, row6size9, row7size9, row8size9, row9size9]
         else
           ["Tamanho de tabuleiro inválido."]
@@ -38,13 +39,13 @@ defmodule RelationsBoard do
     end
   end
 
-  def take_pipe_out([]), do: []
+  def take_pipe_out(""), do: []
 
-  def take_pipe_out([x | xs]) when x != '|' do
-    [x | take_pipe_out(xs)]
+  def take_pipe_out(row) do
+    row
+    |> String.graphemes()
+    |> Enum.filter(fn x -> x != "|" end)
   end
-
-  def take_pipe_out([_ | xs]), do: take_pipe_out(xs)
 
   def take_all_comparators_from_row([]), do: []
 
