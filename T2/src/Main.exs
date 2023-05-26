0ba2f555e88547a5cf87d71d19bcd66e503a04ef
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-05-26 16:16:59 -0300

    Fixed RelationsBoard

--- T2/src/Main.exs
+++ T2/src/Main.exs
@@ -1,13 +1,15 @@
 alias SizeConfig, as: SizeConfig
+alias RelationsBoard, as: RelationsBoard
 
 defmodule Main do
 
   board_size = SizeConfig.size_board()
   row_region_size = SizeConfig.size_row_region()
   column_region_size = SizeConfig.size_column_region()
+  relations_matrix = RelationsBoard.get_relations()
 
   IO.puts("Tamanho do tabuleiro: #{board_size}")
   IO.puts("Tamanho das regiões de linha: #{row_region_size}")
   IO.puts("Tamanho das regiões de coluna: #{column_region_size}")
-
+  IO.puts("Matriz de relações: #{relations_matrix}")
 end
